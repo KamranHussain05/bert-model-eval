a9301a669774046521c2ce0196013fe18fd3e3f4
kamranhussain05 <kamran.hssn05@gmail.com>
2022-04-03 18:07:18 -0700

    add new tokenization pipeline

---
+++ Tokenizers/FastTextTokenization.py
@@ -0,0 +1,14 @@
+
+
+import fasttext
+import pandas as pd
+
+# Create the training text file
+df = pd.read_csv('/home/kamran/Documents/bert-model-eval/SentimentTrainingEval/daily_weather_training_data.csv', usecols=['text'])
+df['text'].to_csv('training_text.txt', index=False, header=False, sep='\n')
+
+
+model = fasttext.train_unsupervised(input="training_text.txt", model="cbow", epoch=50, dim=256)
+model.save_model("fasttexttokenizer.bin")
+
+print(model.get_word_vector('clear weather throughout the day'))
